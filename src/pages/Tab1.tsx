fa01c68b89ad9c5fcf56ec45b893a565db5313ac
KennethLicuanan <licuanankennethlloyd05@gmail.com>
2024-03-12 08:26:23 +0800

    Added alert button and action sheet button

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,11 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,
+   IonAlert, IonButton,
+   IonActionSheet } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+     <IonHeader>
         <IonToolbar>
           <IonTitle>Profile</IonTitle>
         </IonToolbar>
@@ -25,6 +27,53 @@ const Tab1: React.FC = () => {
           </IonCardHeader>
 
           <IonCardContent>Honor once bound me above all else.</IonCardContent>
+
+
+          <IonButton id="present-alert" size="small" expand="block">Click Me</IonButton>
+          <IonAlert
+            trigger="present-alert"
+            header="Gwapo Ko?"
+            className="custom-alert"
+            buttons={[
+              {
+                text: 'Of Course',
+                cssClass: 'alert-button-cancel',
+              },
+              {
+                text: 'Yes',
+                cssClass: 'alert-button-confirm',
+              },
+            ]}
+          ></IonAlert>
+
+            <IonButton id="open-action-sheet" expand="block" size='small'>Open</IonButton>
+              <IonActionSheet
+                trigger="open-action-sheet"
+                header="Actions"
+                buttons={[
+                  {
+                    text: 'Delete',
+                    role: 'destructive',
+                    data: {
+                      action: 'delete',
+                    },
+                  },
+                  {
+                    text: 'Share',
+                    data: {
+                      action: 'share',
+                    },
+                  },
+                  {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    data: {
+                      action: 'cancel',
+                    },
+                  },
+                ]}
+              ></IonActionSheet>
+
         </IonCard>
 
       </IonContent>
