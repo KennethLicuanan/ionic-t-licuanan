d52c58f647b50b85104c5ad31ebf256b57112c8b
KennethLicuanan <licuanankennethlloyd05@gmail.com>
2024-03-19 08:25:25 +0800

    Added components on Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import {
+  //Initial Components
+  IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+} from '@ionic/react';
 
 const Home: React.FC = () => {
   return (
@@ -16,8 +17,33 @@ const Home: React.FC = () => {
             <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+
+      <p>Here's a small text description for the content. Nothing more, nothing less.</p>
+    
       </IonContent>
     </IonPage>
+
+
+
+  );
+
+  return (
+    <IonContent className="ion-padding">
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+
+      <p>Here's a small text description for the content. Nothing more, nothing less.</p>
+    </IonContent>
   );
 };
 
