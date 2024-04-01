0ada1c3ece2b30917b76dcd0d321161396d2b851
KennethLicuanan <licuanankennethlloyd05@gmail.com>
2024-04-02 07:58:18 +0800

    added a click counter source code

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,8 +1,17 @@
+import React, { useState } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
+  // State to track the number of clicks
+  const [clickCount, setClickCount] = useState(0);
+
+  // Function to handle click events and increment the counter
+  const handleClick = () => {
+    setClickCount(prevCount => prevCount + 1);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,6 +25,12 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Click Counter</IonTitle>
           </IonToolbar>
         </IonHeader>
+        {/* Display the click counter */}
+        <div className="click-counter">
+          <p>Number of Clicks: {clickCount}</p>
+          {/* Button to increment the counter */}
+          <button onClick={handleClick}>Click me!</button>
+        </div>
       </IonContent>
     </IonPage>
   );
