f7c04156a6bf79fff54e99b95c496ecfaeb44951
KennethLicuanan <licuanankennethlloyd05@gmail.com>
2024-04-19 09:07:28 +0800

    added src code for todolist, and added css on todolist.css

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,28 +1,98 @@
+import React, { useState, useEffect } from 'react';
 import {
-  //Initial Components
-  IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonCheckbox,
+  IonLabel,
+  IonInput,
+  IonButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardContent
 } from '@ionic/react';
 import './TodoList.css';
+import { addCircleOutline, checkmarkCircleOutline } from 'ionicons/icons';
 
 const Home: React.FC = () => {
+  const [todos, setTodos] = useState<string[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  useEffect(() => {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  const handleToggleTodo = (index: number) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index] = updatedTodos[index] + ' - Completed';
+    setTodos(updatedTodos);
+  };
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar color="primary">
           <IonTitle class='home'>Todo List</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Todo List</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        <IonGrid>
+          <IonRow>
+            <IonCol size="12" size-md="8" offset-md="2">
+              <IonCard>
+                <IonCardContent>
+                  <IonList>
+                    {todos.map((todo, index) => (
+                      <IonItem key={index} lines="none">
+                        <IonCheckbox slot="start" onIonChange={() => handleToggleTodo(index)} />
+                        <IonLabel>{todo}</IonLabel>
+                        <IonIcon icon={checkmarkCircleOutline} slot="end" color="success" />
+                      </IonItem>
+                    ))}
+                  </IonList>
+                  <IonItem lines="none">
+                    <IonInput
+                      value={newTodo}
+                      placeholder="Enter a new todo"
+                      onIonChange={(e) => setNewTodo(e.detail.value!)}
+                    />
+                    <IonButton slot="end" onClick={handleAddTodo} fill="clear">
+                      <IonIcon icon={addCircleOutline} slot="icon-only" />
+                    </IonButton>
+                  </IonItem>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+        <br />
+        <br />
+        <br />
+        <IonButton href='/Home'>Back To Home</IonButton>
       </IonContent>
     </IonPage>
   );
 };
-
-
 
 export default Home;
